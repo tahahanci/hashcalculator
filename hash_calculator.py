6e1a0fbfe9b1309497ece471aca9159cfaeb883c
Taha Hancı <taha.hanci@boun.edu.tr>
2024-06-08 00:19:19 +0300

    virustotal

--- hash_calculator.py
+++ hash_calculator.py
@@ -2,6 +2,7 @@
 
 import hashlib
 import argparse
+import requests
 
 
 def calculate_file_hash(file):
@@ -17,9 +18,33 @@ def calculate_file_hash(file):
         return f"An error occurred: {e}"
 
 
+def check_virustotal(hash_value, api_key):
+    url = f"https://www.virustotal.com/api/v3/files/{hash_value}"
+    headers = {
+        "x-apikey": api_key,
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f"An error occurred: {response.text}"
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate the hash of a file.")
     parser.add_argument("--file", help="The file to calculate the hash of.")
+    parser.add_argument("--api-key", help="The VirusTotal API key.")
     args = parser.parse_args()
+
     file_path = args.file
-    print(calculate_file_hash(file_path))
+    api_key = args.api_key
+    file_hash = calculate_file_hash(file_path)
+
+    print(f"Hash of the file: {file_hash}")
+
+    if "File cannot found!" not in file_hash and "An error occurred" not in file_hash:
+        response = check_virustotal(file_hash, api_key)
+        print(response)
+    else:
+        print(file_hash)
