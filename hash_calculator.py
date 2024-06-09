c1cf6338d155e24c100c3c378594b5f95dee110a
Taha Hancı <taha.hanci@boun.edu.tr>
2024-06-09 11:16:40 +0300

    urlscan.io added

--- hash_calculator.py
+++ hash_calculator.py
@@ -57,6 +57,26 @@ def resolve_short_url(short_url):
         return f"An error occurred: {e}"
 
 
+def query_urlscan(url, urlscan_api_key):
+    url = "https://urlscan.io/api/v1/scan/"
+    headers = {
+        "API-Key": urlscan_api_key,
+        "Content-Type": "application/json",
+    }
+    data = {
+        "url": url,
+        "visibility": "public"
+    }
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"An error occurred: {response.text}"
+    except requests.RequestException as e:
+        return f"Error querying urlscan.io:: {e}"
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="File hash calculator, VirusTotal checker, and IP info fetcher")
     parser.add_argument('--file', type=str, help="Path to the file to calculate the hash")
@@ -64,6 +84,8 @@ if __name__ == "__main__":
     parser.add_argument('--ip', type=str, help="IP address to fetch information")
     parser.add_argument('--ipinfo-token', type=str, help="Token for ipinfo.io API")
     parser.add_argument('--url', type=str, help='Short URL to resolve to the final URL')
+    parser.add_argument('--urlscan-api-key', type=str, help="API key for urlscan.io")
+    parser.add_argument('--scan-url', type=str, help="URL to scan with urlscan.io")
     args = parser.parse_args()
 
     if args.file and args.apikey:
@@ -91,6 +113,14 @@ if __name__ == "__main__":
             print(f"Short URL {args.url} redirects to: {final_url}")
         else:
             print(f"Failed to resolve the short URL: {args.url}")
+    elif args.scan_url and args.urlscan_api_key:
+        print(f"Scanning URL with urlscan.io: {args.scan_url}")
+        scan_result = query_urlscan(args.scan_url, args.urlscan_api_key)
+        if scan_result:
+            print(f"URL scan results for {args.scan_url}:")
+            print_json_pretty(scan_result)
+        else:
+            print(f"Failed to scan the URL: {args.scan_url}")
     else:
         print("Please provide either --file and --apikey for hash calculation "
               "and VirusTotal check, or --ip and --ipinfo-token for IP info.")
